3f85faaf2eb80103d4e2814945eb1bbb678d1ed4
root <root@beaglebone.localdomain>
2015-05-06 05:52:48 +0000

    debug grove_digital_light_sensor

--- Software/Python/Grove_I2C_ADC.py
+++ Software/Python/Grove_I2C_ADC.py
@@ -1,33 +1,32 @@
 from Adafruit_I2C import Adafruit_I2C
 import time
 
+ADDR_ADC121 = 0x50
+
+REG_ADDR_RESULT = 0x00
+REG_ADDR_ALERT = 0x01
+REG_ADDR_CONFIG = 0x02
+REG_ADDR_LIMITL = 0x03
+REG_ADDR_LIMITH = 0x04
+REG_ADDR_HYST = 0x05
+REG_ADDR_CONVL = 0x06
+REG_ADDR_CONVH = 0x07
+
 getData = int()
 analogVal = int()
 
-i2c = Adafruit_I2C(0x55)        
-i2c.write8(0x02, 0x20)
+i2c = Adafruit_I2C(ADDR_ADC121)        
 
 class I2C_ADC():
     def __init__(self):
-        self.ADDR_ADC121 = 0x55
-        self.V_REF = 3.00
-        self.REG_ADDR_RESULT = 0x00
-        self.REG_ADDR_ALERT = 0x01
-        self.REG_ADDR_CONFIG = 0x02
-        self.REG_ADDR_LIMITL = 0x03
-        self.REG_ADDR_LIMITH = 0x04
-        self.REG_ADDR_HYST = 0x05
-        self.REG_ADDR_CONVL = 0x06
-        self.REG_ADDR_CONVH = 0x07
+        i2c.write8(REG_ADDR_CONFIG, 0x20)
         
-    def read_adc(self):
-        data = 0x0000
-        data = i2c.readU16(self.REG_ADDR_RESULT)
-        time.sleep(0.1)
+    def read_adc(self):     
+        data = i2c.readS16(REG_ADDR_RESULT)      
         return data
         
 if __name__=="__main__":
+    adc = I2C_ADC()
     while True:
-	adc = I2C_ADC()
-	print 'sensor value ', float(adc.read_adc() / 655.35), '%'
-        time.sleep(1)        
+        print 'sensor value ', adc.read_adc()
+        time.sleep(.2)        
