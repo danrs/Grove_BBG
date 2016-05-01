5f35d70b04ea42057bd9888123caa7a53fb1c5b1
Daniel Smith <danrs@users.noreply.github.com>
2016-05-01 04:14:58 +0000

    fixed gps demo up

--- Software/Python/Grove_GPS.py
+++ Software/Python/Grove_GPS.py
@@ -11,15 +11,16 @@ ser = serial.Serial(port = "/dev/ttyO2", baudrate=9600)
 ser.flush()
 
 class GPS:
-    #The GPS module used is a Grove GPS module http://www.seeedstudio.com/depot/Grove-GPS-p-959.html
-    inp=[]
+    # The GPS module used is a Grove GPS module http://www.seeedstudio.com/depot/Grove-GPS-p-959.html
     # Refer to SIM28 NMEA spec file http://www.seeedstudio.com/wiki/images/a/a0/SIM28_DATA_File.zip
-    GGA=[]
+    inp=[] #input string
+    GGA=[] #parsed data
 
     #Read data from the GPS
-    def read(self):    
+    def read(self):
         while True:
             GPS.inp=ser.readline()
+            print GPS.inp
             if GPS.inp[:6] =='$GPGGA': # GGA data , packet 1, has all the data we need
                 break
             time.sleep(0.1)
@@ -30,18 +31,18 @@ class GPS:
             print ""
         GPS.GGA=GPS.inp.split(",")    #Split the stream into individual parts
         return [GPS.GGA]
-        
+
     #Split the data into individual elements
     def vals(self):
-        time=GPS.GGA[1]
+        time=GPS.GGA[1] #UTC time
         lat=GPS.GGA[2]
         lat_ns=GPS.GGA[3]
-        long=GPS.GGA[4]
-        long_ew=GPS.GGA[5]
+        lon=GPS.GGA[4]
+        lon_ew=GPS.GGA[5]
         fix=GPS.GGA[6]
         sats=GPS.GGA[7]
         alt=GPS.GGA[9]
-        return [time,fix,sats,alt,lat,lat_ns,long,long_ew]
+        return [time,fix,sats,alt,lat,lat_ns,lon,lon_ew]
 
 g=GPS()
 f=open("gps_data.csv",'w')    #Open file to log the data
@@ -50,10 +51,11 @@ ind=0
 while True:
     try:
         x=g.read()    #Read from GPS
-        [t,fix,sats,alt,lat,lat_ns,long,long_ew]=g.vals()    #Get the individial values
-        print "Time:",t,"Fix status:",fix,"Sats in view:",sats,"Altitude",alt,"Lat:",lat,lat_ns,"Long:",long,long_ew
-        s=str(t)+","+str(float(lat)/100)+","+str(float(long)/100)+"\n"    
-        f.write(s)    #Save to file
+        [t,fix,sats,alt,lat,lat_ns,lon,lon_ew]=g.vals()    #Get the individial values
+        print "Time:",t,"Fix status:",fix,"Sats in view:",sats,"Altitude",alt,"Lat:",lat,lat_ns,"Long:",lon,lon_ew
+        if lat is not "" and lon is not "":
+            s=str(t)+","+str(float(lat)/100)+","+str(float(lon)/100)+"\n"
+            f.write(s)    #Save to file
         time.sleep(2)
     except IndexError:
         print "Unable to read"
@@ -61,5 +63,5 @@ while True:
         f.close()
         print "Exiting"
         sys.exit(0)
-        
+
 
